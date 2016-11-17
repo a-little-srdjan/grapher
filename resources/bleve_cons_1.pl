2900e6b9d93a9c1b79392077a65a2cfa8c882d3f
a-little-srdjan <srdjan.marinovic@gmail.com>
2016-11-17 18:36:45 -0500

    cons

--- resources/bleve_cons_1.pl
+++ resources/bleve_cons_1.pl
@@ -1,9 +1,2 @@
-mark(1, "github.com/blevesearch/bleve").
-mark(1, "github.com/blevesearch/bleve/index").
-mark(2, "github.com/blevesearch/bleve/index/smolder").
-mark(2, "github.com/blevesearch/bleve/index/store").
-mark(2, "github.com/blevesearch/bleve/index/upsidedown").
 
-exception("github.com/blevesearch/bleve/index", "github.com/blevesearch/bleve/index/smolder").
-exception("github.com/blevesearch/bleve/index", "github.com/blevesearch/bleve/index/store").
-exception("github.com/blevesearch/bleve/index", "github.com/blevesearch/bleve/index/upsidedown").
+violation(Y) :- dependency("github.com/blevesearch/bleve/search", Y), nested("github.com/blevesearch/bleve/index", Y). 
